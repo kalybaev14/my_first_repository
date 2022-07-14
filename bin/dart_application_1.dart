2ce3f5fe35b7ba6f15476835e14c5fdba5e5c5d3
kalybaev14 <kalybaevajnabek@gmail.com>
2022-07-14 23:25:27 +0300

    added new files

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,15 @@
+import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
+
+void main(List<String> arguments) {
+  double b=4.20;
+  int a=3;
+  print(b+a);
+  String text='Name';
+  text='Flutter';
+  print('I want to learn '+text);
+  double myNumber=2.225;
+  String myText;
+  myText=myNumber.toString();
+
+  print(myText);
+}
